16b4a3b7e637930cde435462182ba0a3cc51a45b
Arsalan Khan <arsalan@local.com>
2020-02-18 19:53:22 +0500

    adding ansible script for one zookeeper container run

--- terraform/kafka-zk/outputs.tf
+++ terraform/kafka-zk/outputs.tf
@@ -1,3 +1,11 @@
 output "kafka_cluster_ips" {
     value = "${module.kafka_cluster.public_ips}"
 }
+
+output "kafka_cluster_private_ips" {
+    value = "${module.kafka_cluster.private_ips}"
+}
+output "vpc_name" {
+  value = "${module.vpc.vpc_id}"
+}
+
