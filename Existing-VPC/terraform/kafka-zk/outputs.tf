0da16fde810695bba4dca741a2a2c127a6465ec6
Arsalan Khan <arsalankhan@devops.com>
2020-09-27 15:41:32 +0500

    replicate to existing vpc

--- Existing-VPC/terraform/kafka-zk/outputs.tf
+++ Existing-VPC/terraform/kafka-zk/outputs.tf
@@ -9,3 +9,12 @@ output "private_ips" {
 output "exhibitor_s3_bucket" {
   value = module.s3_bucket.this_s3_bucket_id
 }
+
+resource "local_file" "Ansible-Inventory" {
+    content = templatefile("inventory.tmpl",
+    {
+        ansible_host = module.kafka_cluster.public_ips,
+        private_ip   = module.kafka_cluster.private_ips
+    })
+    filename = "../../ansible/dyn_inven"
+}
